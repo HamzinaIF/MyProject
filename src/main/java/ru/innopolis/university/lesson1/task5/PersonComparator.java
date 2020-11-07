6a15d28ca5aa6897539a3ee4695c09fb3f108239
Hamzina-IF <ilzirik90@mail.ru>
2020-11-07 14:54:49 +0500

    lesson1

--- src/main/java/ru/innopolis/university/lesson1/task5/PersonComparator.java
+++ src/main/java/ru/innopolis/university/lesson1/task5/PersonComparator.java
@@ -1,6 +1,6 @@
 package ru.innopolis.university.lesson1.task5;
 import java.util.*;
-abstract class PersonComparator implements Comparator<Person> {
+abstract class PersonComparator implements Comparator<Person> {//Компаратор
     public static Comparator<Person> comparator = Comparator.comparing(person -> person.getSex());
     public static Comparator<Person> comparator1 = comparator.thenComparing(person -> person.getAge());
     public static Comparator<Person> comparator2 = comparator1.thenComparing(person -> person.getName());
